db6f0ccaab0e0556b32c17eac738324ffe7f981e
aperbet56 <103628998+aperbet56@users.noreply.github.com>
2025-01-23 18:23:43 +0100

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -17,7 +17,7 @@ const CONFIG = {
       mode: 'automatic', // Mode can be: 'automatic' or 'manual'
       automatic: {
         sortBy: 'stars', // Sort projects by 'stars' or 'updated'
-        limit: none, // How many projects to display.
+        limit: 24, // How many projects to display.
         exclude: {
           forks: false, // Forked projects will not be displayed if set to true.
           projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
